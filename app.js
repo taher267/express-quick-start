5e5bb79f602576056492d9d879a8c31ec1170cd9
ABU TAHER <abutaher267@gmail.com>
2022-07-17 17:04:38 +0600

    add 1

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 import Express from 'Express';
 import { config } from 'dotenv';
 config({ path: './config/config.env' });
-import coockieParser from 'coockie-parser';
+import cookieParser from 'cookie-parser';
 // import session from 'express-session';
 import cors from 'cors';
+// import mongan from 'mongan';
 import { createServer } from 'http';
 import DBConnect from './config/db';
 import routers from './routers';
+import globalErrHandler from './middlewires/error';
 
 const app = Express();
 const middlewires = [
     Express.json({ limit: process.env.JSON_FILE_LIMIT }),
     cors(),
     Express.urlencoded({ extended: true, limit: process.env.URLENCODED_LIMIT }),
-    coockieParser(),
+    cookieParser(),
+    globalErrHandler
 ];
 
 app.use(middlewires);
@@ -27,17 +30,11 @@ app.use('*', (req, res, _n) => {
     res.status(404).json(`${req.path} not found`);
 });
 
-app.use((err, _rq, res, _n) => {
-    console.log('err Name', err.message);
-    const message = err.message || `ServeR ErroR OccereD`;
-    let status = err.status || 500;
-    if (err.name === "MongoServerError") status = 500;
 
-    return res.status(status).json({
-        message
-    });
-});
-DBConnect();
+if (process.env.NODE_ENV === 'development') app.use(mongan);
+//Database Connection
+if (process.env.MONGO_URI) DBConnect();
+
 const PORT = process.env.PORT || 1111;
 const server = httpServer.listen(PORT, _ => {
     console.log(`Alhamdu lillah server is listening on port ${PORT}`);
--- readme.txt
+++ readme.txt
@@ -1,5 +1,6 @@
 mkdir models middlewires services controllers config utils public routers
 
 
-yarn add express dotenv cross-env cookie-parser express-validator express-session mongoose esm bcryptjs jsonwebtoken nodemailer cors 
+yarn add express dotenv cross-env cookie-parser express-validator express-session mongoose esm bcryptjs jsonwebtoken nodemailer cors
+
 yarn add -D morgan
